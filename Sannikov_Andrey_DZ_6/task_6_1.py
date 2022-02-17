c37e44f61a5780fed8824d9780431db59a5cfb03
AndreySan127 <sannikov26@gmail.com>
2022-02-17 12:02:38 +0400

    DZ_6_1

--- Sannikov_Andrey_DZ_6/task_6_1.py
+++ Sannikov_Andrey_DZ_6/task_6_1.py
@@ -1,13 +1,14 @@
-def parse_log(pth_file):
+
+def get_parse_attrs(pth_file)-> tuple:
 
     if pth_file:
-        with open(pth_file,"r",encoding="utf-8") as file:
+        with open(pth_file,'r', encoding='utf-8') as file:
             for line in file:
                 ip = line.split(" - - ")[0]
                 rsp_and_pth = line.split('"')[1]
                 rsp = rsp_and_pth.split()[0]
                 pth = rsp_and_pth.split()[1]
-                yield(ip, rsp, pth)
+                yield ip, rsp, pth
 
 
 def find_spamer(parsed_log):
@@ -15,6 +16,6 @@ def find_spamer(parsed_log):
 
 
 if __name__ == "__main__":
-   a = parse_log("./nginx_logs.txt")
+   a = get_parse_attrs("./nginx_logs.txt")
    for e in a:
        print(e)
